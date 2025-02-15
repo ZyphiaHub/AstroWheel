170587ef2b48aa64c2a6a01f107d52b437bfc60a
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2025-02-15 20:00:28 +0100

    next prev gomb

--- Assets/Scripts/IslandManager.cs
+++ Assets/Scripts/IslandManager.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class IslandManager : MonoBehaviour, IGameState {
     public int islandIndex; // A sziget indexe (pl. 1, 2, ..., 12)
     public DialogManager dialogManager;
     public Button bactToMainMenuBtn;
+    public Button previousIslandButton; // Elõzõ sziget gombja
+    public Button nextIslandButton;
 
     private void Start()
     {
@@ -14,6 +17,15 @@ public class IslandManager : MonoBehaviour, IGameState {
         {
             bactToMainMenuBtn.onClick.AddListener(OnBackToMainMenuClicked);
         }
+        if (previousIslandButton != null)
+        {
+            previousIslandButton.onClick.AddListener(OnPreviousIslandClicked);
+        }
+        if (nextIslandButton != null)
+        {
+            nextIslandButton.onClick.AddListener(OnNextIslandClicked);
+        }
+
         // Állapot inicializálása
         EnterState();
     }
@@ -33,11 +45,12 @@ public class IslandManager : MonoBehaviour, IGameState {
             dialogManager.dialogPanel.SetActive(false);
             
         }
+        UpdateNavigationButtons();
     }
 
     public void UpdateState()
     {
-        // Itt ellenõrizheted, hogy a puzzle megoldódott-e
+        
         if (IsPuzzleSolved())
         {
             ExitState();
@@ -58,6 +71,45 @@ public class IslandManager : MonoBehaviour, IGameState {
         // Visszalépés a fõmenübe
         GameStateManager.Instance.ChangeState(new MainMenuState());
     }
+
+    private void OnPreviousIslandClicked()
+    {
+        NavigateToIsland(islandIndex - 1);
+    }
+
+    // Gomb eseménykezelõje: Következõ sziget
+    private void OnNextIslandClicked()
+    {
+        NavigateToIsland(islandIndex + 1);
+    }
+
+    private void NavigateToIsland(int targetIslandIndex)
+    {
+        if (targetIslandIndex >= 1 && targetIslandIndex <= 12) 
+        {
+            Debug.Log($"Navigálás a(z) {targetIslandIndex}. szigetre!");
+            SceneManager.LoadScene($"Island_{targetIslandIndex}"); 
+        } else
+        {
+            Debug.LogWarning($"Érvénytelen sziget index: {targetIslandIndex}");
+        }
+    }
+
+    private void UpdateNavigationButtons()
+    {
+        // Elõzõ sziget gomb letiltása, ha az elsõ szigeten vagyunk
+        if (previousIslandButton != null)
+        {
+            previousIslandButton.interactable = (islandIndex > 1);
+        }
+
+        // Következõ sziget gomb letiltása, ha az utolsó szigeten vagyunk
+        if (nextIslandButton != null)
+        {
+            nextIslandButton.interactable = (islandIndex < 12);
+        }
+    }
+
     // Puzzle megoldásának ellenõrzése
     private bool IsPuzzleSolved()
     {
