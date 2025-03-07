073c70ee13224941c76986a728d1790c89752524
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2025-03-07 18:41:13 +0100

    adatokmainmenube

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour {
-    public Button[] islandButtons; 
+    public Button[] islandButtons;
+
+    public TMP_Text playerNameText; // Játékos neve
+    public TMP_Text playerScoreText; // Pontok
+    public TMP_Text lastCompletedIslandText; // Utolsó teljesített sziget száma
 
     private void Start()
     {
         Debug.Log("Beléptél a Fõmenübe!");
+        // Adatok betöltése és megjelenítése
+        LoadAndDisplayPlayerData();
         // Gombok állapotának beállítása
         for (int i = 0; i < islandButtons.Length; i++)
         {
@@ -28,9 +35,24 @@ public class MainMenuManager : MonoBehaviour {
             
         }
     }
-        public void LoadIslandScene(int islandIndex)
+    public void LoadIslandScene(int islandIndex)
     {
         // Sziget scene betöltése
-        SceneManager.LoadScene($"Island_{islandIndex}");
+        SceneManager.LoadScene($"Island_{islandIndex}"); }
+
+        private void LoadAndDisplayPlayerData()
+    {
+        // Játékos neve
+        string playerName = PlayerPrefs.GetString("PlayerName", "Guest");
+        playerNameText.text = "Player: " + playerName;
+
+        // Pontok
+        int playerScore = PlayerPrefs.GetInt("PlayerScore", 0);
+        playerScoreText.text = "TimeScore: " + playerScore;
+
+        // Utolsó teljesített sziget száma
+        int lastCompletedIsland = PlayerPrefs.GetInt("LastCompletedIsland", 0);
+        lastCompletedIslandText.text = "Last Completed Island: " + lastCompletedIsland;
     }
+
 }
