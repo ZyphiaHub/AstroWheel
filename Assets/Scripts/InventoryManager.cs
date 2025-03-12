9c3c62229a1aaea7703198b1e9890ae6b4bbfea1
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2025-03-12 10:32:35 +0100

    Update InventoryManager.cs

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -5,9 +5,11 @@ public class InventoryManager : MonoBehaviour {
     public static InventoryManager Instance { get; private set; }
 
     public Inventory inventory { get; private set; }
+    public CraftedInventory craftedInventory { get; private set; } // Új CraftedInventory
 
     [Header("References")]
     public PlantDatabase plantDatabase; // Az Inspectorban állítsd be!
+    public ItemDatabase itemDatabase; // Új ItemDatabase referenciája
 
     private void Awake()
     {
@@ -21,43 +23,90 @@ public class InventoryManager : MonoBehaviour {
             return;
         }
 
-        InitializeInventory();
+        InitializeInventories();
     }
 
-    //  Inventory inicializálása és betöltése
-    private void InitializeInventory()
+    // Inventoryk inicializálása és betöltése
+    private void InitializeInventories()
     {
+        // Plant inventory inicializálása
         inventory = new Inventory();
+        bool hasSavedPlantData = false;
 
-        bool hasSavedData = false;
-
-        // Ellenõrizzük, van-e mentett adat
         foreach (var item in plantDatabase.items)
         {
             string key = "plant_" + item.englishName.Replace(" ", "");
             if (PlayerPrefs.HasKey(key))
             {
-                hasSavedData = true;
+                hasSavedPlantData = true;
                 break;
             }
         }
 
-        if (hasSavedData)
+        if (hasSavedPlantData)
         {
             LoadInventory();
         } else
         {
-            // Ha nincs mentett adat, inicializáljuk 0 értékekkel és mentjük
             foreach (var item in plantDatabase.items)
             {
                 inventory.AddItem(item, 0);
             }
             SaveInventory();
-            Debug.Log("New inventory initialized with 0 quantities.");
+            Debug.Log("New plant inventory initialized with 0 quantities.");
         }
+
+        // Crafted inventory inicializálása
+        craftedInventory = new CraftedInventory();
+        LoadCraftedInventory();
+
+        // Ellenõrizzük, hogy minden tárgy szerepel-e a CraftedInventory-ben
+        foreach (var item in itemDatabase.items)
+        {
+            if (!craftedInventory.items.ContainsKey(item))
+            {
+                // Ha nincs benne, hozzáadjuk 0 mennyiséggel
+                craftedInventory.AddItem(item, 0);
+                Debug.Log($"New item added to CraftedInventory: {item.itemName}");
+            }
+        }
+
+        // Mentjük a frissített CraftedInventory-t
+        SaveCraftedInventory();
+        Debug.Log("Crafted inventory initialized with new items.");
+
+        /*bool hasSavedCraftedData = false;
+        
+        foreach (var item in itemDatabase.items)
+        {
+            string key = "crafted_" + item.itemName.Replace(" ", "");
+            if (PlayerPrefs.HasKey(key))
+            {
+                hasSavedCraftedData = true;
+                break;
+            }
+        }
+
+        if (hasSavedCraftedData)
+        {
+            LoadCraftedInventory();
+            
+            // Mentjük a frissített CraftedInventory-t
+            SaveCraftedInventory();
+            Debug.Log("Crafted inventory initialized with new items.");
+
+        } else
+        {
+            foreach (var item in itemDatabase.items)
+            {
+                craftedInventory.AddItem(item, 0);
+            }
+            SaveCraftedInventory();
+            Debug.Log("New crafted inventory initialized with 0 quantities.");
+        }*/
     }
 
-    //  Inventory mentése PlayerPrefs-be
+    // Plant inventory mentése PlayerPrefs-be
     public void SaveInventory()
     {
         foreach (var entry in plantDatabase.items)
@@ -68,10 +117,34 @@ public class InventoryManager : MonoBehaviour {
         }
 
         PlayerPrefs.Save();
-        Debug.Log("Inventory saved.");
+        Debug.Log("Plant inventory saved.");
     }
 
-    //  Inventory betöltése PlayerPrefs-bõl
+    // Crafted inventory mentése PlayerPrefs-be
+    public void SaveCraftedInventory()
+    {
+
+        foreach (var entry in itemDatabase.items)
+        {
+            string key = "crafted_" + entry.itemName.Replace(" ", "");
+            int quantity = craftedInventory.items.ContainsKey(entry) ? craftedInventory.items[entry] : 0;
+            PlayerPrefs.SetInt(key, quantity);
+        }
+
+        PlayerPrefs.Save();
+        Debug.Log("Crafted inventory saved.");
+        /*foreach (var entry in itemDatabase.items)
+        {
+            string key = "crafted_" + entry.itemName.Replace(" ", "");
+            int quantity = craftedInventory.items.ContainsKey(entry) ? craftedInventory.items[entry] : 0;
+            PlayerPrefs.SetInt(key, quantity);
+        }
+
+        PlayerPrefs.Save();
+        Debug.Log("Crafted inventory saved.");*/
+    }
+
+    // Plant inventory betöltése PlayerPrefs-bõl
     public void LoadInventory()
     {
         foreach (var item in plantDatabase.items)
@@ -83,13 +156,55 @@ public class InventoryManager : MonoBehaviour {
                 inventory.AddItem(item, quantity);
             }
         }
-        Debug.Log("Inventory loaded.");
+        Debug.Log("Plant inventory loaded.");
     }
 
-    //  PlayerPrefs törlése 
+    // Crafted inventory betöltése PlayerPrefs-bõl
+    public void LoadCraftedInventory()
+    {
+
+        foreach (var item in itemDatabase.items)
+        {
+            string key = "crafted_" + item.itemName.Replace(" ", "");
+            if (PlayerPrefs.HasKey(key))
+            {
+                int quantity = PlayerPrefs.GetInt(key);
+                craftedInventory.items[item] = quantity; // Frissítjük a mennyiséget
+            }
+        }
+        Debug.Log("Crafted inventory loaded.");
+        /*foreach (var item in itemDatabase.items)
+        {
+            string key = "crafted_" + item.itemName.Replace(" ", "");
+            if (PlayerPrefs.HasKey(key))
+            {
+                int quantity = PlayerPrefs.GetInt(key);
+                craftedInventory.AddItem(item, quantity);
+            }
+        }
+        Debug.Log("Crafted inventory loaded.");*/
+    }
+
+    // PlayerPrefs törlése (mindkét inventoryhoz)
     public void ClearPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
         Debug.Log("PlayerPrefs cleared.");
     }
 }
+
+/*// Tárgyak hozzáadása a leltárakhoz
+InventoryManager.Instance.inventory.AddItem(plantDatabase.items[0], 5); // 5 növény hozzáadása
+InventoryManager.Instance.craftedInventory.AddItem(itemDatabase.items[0], 2); // 2 elkészített tárgy hozzáadása
+
+// Leltárak mentése
+InventoryManager.Instance.SaveInventory();
+InventoryManager.Instance.SaveCraftedInventory();
+
+// Leltárak betöltése
+InventoryManager.Instance.LoadInventory();
+InventoryManager.Instance.LoadCraftedInventory();
+
+// Leltárak tartalmának kiírása
+InventoryManager.Instance.inventory.PrintInventory();
+InventoryManager.Instance.craftedInventory.PrintInventory();*/
